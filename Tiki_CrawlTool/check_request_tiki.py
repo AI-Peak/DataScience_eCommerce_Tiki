8d11f4c5e1be75a662246079d672a9d472e7d1f1
Dacekey <in4.tech.store@gmail.com>
2026-01-23 22:38:36 +0700

    Change Tiki_CrawlingTool

--- Tiki_CrawlTool/check_request_tiki.py
+++ Tiki_CrawlTool/check_request_tiki.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # Change the url to check request
-url = "https://tiki.vn/nha-sach-tiki/c8322"
+url = "https://tiki.vn/the-thao-da-ngoai/c1975"
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) Gecko/20100101 Firefox/147.0",
     "Accept-Language": "vi-VN,vi;q=0.9,en;q=0.8",
